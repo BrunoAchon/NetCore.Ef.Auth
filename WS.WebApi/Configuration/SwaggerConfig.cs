e2c987cb4d42264d6b992accf288ed633d1dddba
Bruno Achon Sanches <brunoachon@hotmail.com>
2021-12-02 13:53:54 -0300

    Collapse do Swagger

--- WS.WebApi/Configuration/SwaggerConfig.cs
+++ WS.WebApi/Configuration/SwaggerConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.IO;
 using System.Reflection;
@@ -14,14 +15,14 @@ namespace WS.WebApi.Configuration
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", 
-                    new OpenApiInfo 
-                    { 
-                        Title = "WebSgm - Sistema de Gestão de Multas.", 
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "WebSgm - Sistema de Gestão de Multas.",
                         Version = "v1",
                         Description = "Api geral do sistema de gestão de multas.",
                     });
-
+                
                 c.AddFluentValidationRules();                
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
@@ -38,6 +39,7 @@ namespace WS.WebApi.Configuration
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
+                c.DocExpansion(DocExpansion.None);
                 c.RoutePrefix = string.Empty;
                 c.SwaggerEndpoint("./swagger/v1/swagger.json", "WS v1");
             });
